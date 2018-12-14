0fedaac0c3f8d3d10d1dd6fc838365a03267b7bd
Rodo Andrade <rodo.andrade00111@gmail.com>
2018-12-14 16:06:35 -0500

    A lot of changes
    Increased the number of stars to drop to be based on the width of the screen;
    Cleaned up a lot of code;
    Added Alg2 folder with smaller size (to fit the game size ratio);
    Added so Algs will appear dynamically based on the size of the screen;
    Added Archer as main player, added a lot of Player Assets;
    Added animation and movement for main player archer;

--- FirstAndLast.js
+++ FirstAndLast.js
@@ -10,8 +10,8 @@ var stars;
 var score = 0;
 var scoreText;
 var bombs;
-var groundHeight = 73;
-var groundWidth = 110;
+var groundHeight = 64;
+var groundWidth = 64;
 var IsGameOver = false;
 var originalPlayerWidth = 32;
 var originalPlayerHeight = 48;
@@ -48,7 +48,6 @@ var game = new Phaser.Game(config);
 function preload() {
     //Sky, ground, etc, is the link in the code when creating game objects    
     this.load.image('background', './Assets/unity3d-assets/TooCubeForest/backgrounds/BG_cave2_1024.png');
-    this.load.image('ground', './Assets/images/ground.png');
     this.load.image('star', './Assets/images/star.png');
     this.load.image('bomb', './Assets/images/green-orb-collect.png');
     this.load.spritesheet('dude',
@@ -56,10 +55,72 @@ function preload() {
         { frameWidth: 32, frameHeight: 48 }
     );
 
+    this.load.spritesheet('ground',
+        './Assets/unity3d-assets/TooCubeForest/images/Forest_terrain_red_128px.png',
+        { frameWidth: 64, frameHeight: 64 }
+    );
+
+    this.load.spritesheet('floatingGround1',
+        './Assets/unity3d-assets/TooCubeForest/images/Forest_deco_covers(spring).png',
+        { frameWidth: 129, frameHeight: 41 }
+    );
+
+    //Load animated plants
+    for (var i = 0; i <= 60; i++) {
+        this.load.image('alg' + i, './Assets/unity3d-assets/2D-Handcrafted-Art/Sprite/Alg2/Algae1-animation_' + i + '.png')
+    }
+
+    //Load sprites for Main Player
+    MainPlayerLoad(this);
+
     //Audio
     this.load.audio('collectingSound', './Assets/phaser-assets/audio/SoundEffects/p-ping.mp3');
     this.load.audio('jump', './Assets/audio/bounce.wav');
     this.load.audio('gameMusic', './Assets/audio/Pamgaea.ogg');
+}
+
+function MainPlayerLoad(parent) {
+    //Load animated plants
+
+    for (var i = 0; i <= 14; i++) {
+        parent.load.image('mainplayer-attack1-left' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Attack1_' + i + 'left.png');
+        parent.load.image('mainplayer-attack1-right' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Attack1_' + i + 'right.png');
+    }
+
+    for (var i = 0; i <= 14; i++) {
+        parent.load.image('mainplayer-attack2-left' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Attack2_' + i + 'left.png');
+        parent.load.image('mainplayer-attack2-right' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Attack2_' + i + 'right.png');
+    }
+
+    for (var i = 0; i <= 14; i++) {
+        parent.load.image('mainplayer-death-left' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Death_' + i + 'left.png');
+        parent.load.image('mainplayer-death-right' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Death_' + i + 'right.png');
+    }
+
+    for (var i = 0; i <= 4; i++) {
+        parent.load.image('mainplayer-hit-left' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Hit_' + i + 'left.png');
+        parent.load.image('mainplayer-hit-right' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Hit_' + i + 'right.png');
+    }
+
+    for (var i = 0; i <= 29; i++) {
+        parent.load.image('mainplayer-idle2-left' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Idle2_' + i + 'left.png');
+        parent.load.image('mainplayer-idle2-right' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Idle2_' + i + 'right.png');
+    }
+
+    for (var i = 0; i <= 14; i++) {
+        parent.load.image('mainplayer-idle1-left' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_idle_' + i + 'left.png');
+        parent.load.image('mainplayer-idle1-right' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_idle_' + i + 'right.png');
+    }
+
+    for (var i = 0; i <= 12; i++) {
+        parent.load.image('mainplayer-jump-left' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Jump_' + i + 'left.png');
+        parent.load.image('mainplayer-jump-right' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_Jump_' + i + 'right.png');
+    }
+
+    for (var i = 0; i <= 14; i++) {
+        parent.load.image('mainplayer-walk-left' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_walk_' + i + 'left.png');
+        parent.load.image('mainplayer-walk-right' + i, './Assets/unity3d-assets/SpritesArchers/Archer2/FantasyArcher_02_walk_' + i + 'right.png');
+    }
 }
 /********************************** PRELOAD *******************************************************/
 
@@ -86,6 +147,29 @@ function Collision(parent) {
     parent.physics.add.overlap(player, stars, CollectStar, null, parent);
 }
 
+function LiveBackground(parent) {
+    var algHeight = 117;
+    var algWidth = 56;
+    var algFrames = [];
+    //Load animated plants
+    for (var i = 0; i <= 60; i++) {
+        algFrames.push({ key: 'alg' + i });
+    }
+
+    //Algs        
+    parent.anims.create({
+        key: 'liveAlg',
+        frames: algFrames,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    parent.add.sprite((centerWidth / 3), windowHeight - algHeight, 'alg0').play('liveAlg');
+    parent.add.sprite(centerWidth, windowHeight - algHeight, 'alg0').play('liveAlg');
+    parent.add.sprite((centerWidth + (algWidth * 5)), windowHeight - algHeight, 'alg0').play('liveAlg');
+    parent.add.sprite((centerWidth + (algWidth * 13)), windowHeight - algHeight, 'alg0').play('liveAlg');
+}
+
 function Platform(parent) {
     var firstHeight = (centerHeight + 300);
     var secondHeight = (centerHeight + (groundHeight * 2))
@@ -96,6 +180,8 @@ function Platform(parent) {
 
     //Platform (where user walks on)
     platforms = parent.physics.add.staticGroup();
+
+    LiveBackground(parent);
 
     //Main ground        
     for (var i = 0; i <= (windowWidth + groundWidth);) {
@@ -112,62 +198,213 @@ function Platform(parent) {
     platforms.create((centerWidth + (groundWidth * 2)), secondHeight, 'ground');
 
     //Right
-    platforms.create(centerWidth + (groundWidth * 2), firstHeight, 'ground');
-    platforms.create(centerWidth + (groundWidth * 4), firstHeight, 'ground');
+    platforms.create(centerWidth + (groundWidth * 2), firstHeight, 'floatingGround1');
+    platforms.create(centerWidth + (groundWidth * 4), firstHeight, 'floatingGround1');
 
     //Left
-    platforms.create(centerWidth - (groundWidth * 1), firstHeight, 'ground');
-    platforms.create(centerWidth - (groundWidth * 4), firstHeight, 'ground');
+    platforms.create(centerWidth - (groundWidth * 1), firstHeight, 'floatingGround1');
+    platforms.create(centerWidth - (groundWidth * 4), firstHeight, 'floatingGround1');
 }
 
 function GameSounnd(parent) {
     parent.sound.add('collectingSound');
     parent.sound.add('jumpSound');
     bgMusic = parent.sound.add('gameMusic');
-    bgMusic.config.loop = true;    
+    bgMusic.config.loop = true;
 
-    bgMusic.play();
+    //TODO: Uncomment this once done testing or Implement a Mute button
+    //bgMusic.play();
 }
 
 function Player(parent) {
+    var attack1FramesLeft = [];
+    var attack1FramesRight = [];
+    var attack2FramesLeft = [];
+    var attack2FramesRight = [];
+    var deathFramesLeft = [];
+    var deathFramesRight = [];
+    var hitFramesLeft = [];
+    var hitFramesRight = [];
+    var idle2FramesLeft = [];
+    var idle2FramesRight = [];
+    var idle1FramesLeft = [];
+    var idle1FramesRight = [];
+    var jumpFramesLeft = [];
+    var jumpFramesRight = [];
+    var walkRightFramesLeft = [];
+    var walkRightFramesRight = [];
+
     //Player
-    player = parent.physics.add.sprite(100, windowHeight - 250, 'dude');
+    // player = parent.physics.add.sprite(100, windowHeight - 250, 'dude');    
+    //Add starting image of player, width, height
+    player = parent.physics.add.sprite(192, 192, 'mainplayer-idle1-left0');
+    player.setDisplaySize(81, 81);
 
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
     player.body.setGravityY(300);
 
-    parent.anims.create({
-        key: 'left',
-        frames: parent.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    parent.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
-        frameRate: 20
-    });
-
-    parent.anims.create({
-        key: 'right',
-        frames: parent.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
     //Set original player size
     originalPlayerWidth = player.displayWidth;
     originalPlayerHeight = player.displayHeight;
+
+    //Frames
+    for (var i = 0; i <= 14; i++) {
+        attack1FramesLeft.push({ key: 'mainplayer-attack1-left' + i });
+        attack1FramesRight.push({ key: 'mainplayer-attack1-right' + i });
+    }
+
+    parent.anims.create({
+        key: 'attack1-left',
+        frames: attack1FramesLeft,
+        frameRate: 10,
+        repeat: -1
+    });
+    parent.anims.create({
+        key: 'attack1-right',
+        frames: attack1FramesRight,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    for (var i = 0; i <= 14; i++) {
+        attack2FramesLeft.push({ key: 'mainplayer-attack2-left' + i });
+        attack2FramesRight.push({ key: 'mainplayer-attack2-right' + i });
+    }
+
+    parent.anims.create({
+        key: 'attack2-left',
+        frames: attack2FramesLeft,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    parent.anims.create({
+        key: 'attack2-right',
+        frames: attack2FramesRight,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    for (var i = 0; i <= 14; i++) {
+        deathFramesLeft.push({ key: 'mainplayer-death-left' + i });
+        deathFramesRight.push({ key: 'mainplayer-death-right' + i });
+    }
+
+    parent.anims.create({
+        key: 'death-left',
+        frames: deathFramesLeft,
+        frameRate: 10,
+        repeat: -1
+    });
+    parent.anims.create({
+        key: 'death-right',
+        frames: deathFramesRight,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    for (var i = 0; i <= 4; i++) {
+        hitFramesLeft.push({ key: 'mainplayer-hit-left' + i });
+        hitFramesRight.push({ key: 'mainplayer-hit-right' + i });
+    }
+
+    parent.anims.create({
+        key: 'hit-left',
+        frames: hitFramesLeft,
+        frameRate: 10,
+        repeat: -1
+    });
+    parent.anims.create({
+        key: 'hit-right',
+        frames: hitFramesRight,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    for (var i = 0; i <= 29; i++) {
+        idle2FramesLeft.push({ key: 'mainplayer-idle2-left' + i });
+        idle2FramesRight.push({ key: 'mainplayer-idle2-right' + i });
+    }
+
+    parent.anims.create({
+        key: 'idle2-left',
+        frames: idle2FramesLeft,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    parent.anims.create({
+        key: 'idle2-right',
+        frames: idle2FramesRight,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    for (var i = 0; i <= 14; i++) {
+        idle1FramesLeft.push({ key: 'mainplayer-idle1-left' + i });
+        idle1FramesRight.push({ key: 'mainplayer-idle1-right' + i });
+    }
+
+    parent.anims.create({
+        key: 'idle1-left',
+        frames: idle1FramesLeft,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    parent.anims.create({
+        key: 'idle1-right',
+        frames: idle1FramesRight,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    for (var i = 0; i <= 12; i++) {
+        jumpFramesLeft.push({ key: 'mainplayer-jump-left' + i });
+        jumpFramesRight.push({ key: 'mainplayer-jump-right' + i });
+    }
+
+    parent.anims.create({
+        key: 'jump-left',
+        frames: jumpFramesLeft,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    parent.anims.create({
+        key: 'jump-right',
+        frames: jumpFramesRight,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    for (var i = 0; i <= 14; i++) {
+        walkRightFramesLeft.push({ key: 'mainplayer-walk-left' + i });
+        walkRightFramesRight.push({ key: 'mainplayer-walk-right' + i });
+    }
+
+    parent.anims.create({
+        key: 'walk-left',
+        frames: walkRightFramesLeft,
+        frameRate: 10,
+        repeat: -1
+    });
+
+    parent.anims.create({
+        key: 'walk-right',
+        frames: walkRightFramesRight,
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 /** Collection **/
 function ItemsToCollect(parent) {
     stars = parent.physics.add.group({
         key: 'star',
-        repeat: 11, //Total (1 default + 11)
-        setXY: { x: 12, y: 0, stepX: 70 }
+        repeat: (Math.round(windowWidth / 100) + 1), //Total (1 default + 11)
+        setXY: { x: 12, y: 0, stepX: 100 }
     });
 
     stars.children.iterate(function (child) {
@@ -250,43 +487,74 @@ function update() {
     GameOver(this);
 }
 
+var IsMainPlayerFacingLeft = false;
+
 function Controller(parent) {
     cursors = parent.input.keyboard.createCursorKeys();
 
     if (!IsGameOver) {
+        //Player Left
         if (cursors.left.isDown) {
+            IsMainPlayerFacingLeft = true;
             player.setVelocityX(-160);
-
-            player.anims.play('left', true);
+            player.anims.play('walk-left', true);
         }
+        //Player Right
         else if (cursors.right.isDown) {
+            IsMainPlayerFacingLeft = false;
             player.setVelocityX(160);
-
-            player.anims.play('right', true);
+            player.anims.play('walk-right', true);
         }
+        //Player Not Moving
         else {
             //Stop moving
             player.setVelocityX(0);
-            player.anims.play('turn');
+            if (IsMainPlayerFacingLeft) {
+                player.anims.play('idle2-left');
+            }
+            else {
+                player.anims.play('idle2-right');
+            }
         }
 
-        //Jump
+        //Player Jump
         if (cursors.up.isDown && player.body.touching.down) {
-            player.setVelocityY(-450);
+            //Although I know I could use a ternary operator, I plan on adding more to these statements
+            if (IsPlayerBig) {
+                player.setVelocityY(-550);
+            }
+            else {
+                player.setVelocityY(-450);
+            }
+            if (IsMainPlayerFacingLeft) {
+                player.anims.play('jump-left');
+            }
+            else {
+                player.anims.play('jump-right');
+            }
+
+            //Jumping sound
             game.sound.play('jump');
         }
 
-        //TODO: Set up special attack (Q)
+        //Special Attack, change Size
         if (parent.specialAttack.isDown) {
-            //Shrink
-            if (IsPlayerBig) {
-                IsPlayerBig = false;
-                player.setDisplaySize(originalPlayerWidth, originalPlayerHeight);
+            // //Shrink
+            // if (IsPlayerBig) {
+            //     IsPlayerBig = false;
+            //     player.setDisplaySize(originalPlayerWidth, originalPlayerHeight);
+            // }
+            // //Grow (the player cant be touching the ground or he will fall through it when he grows)
+            // else if (!IsPlayerBig && !player.body.touching.down) {
+            //     IsPlayerBig = true;
+            //     player.setDisplaySize((originalPlayerWidth + 20), (originalPlayerHeight + 20));
+            // }
+
+            if (IsMainPlayerFacingLeft) {
+                player.anims.play('attack1-left');
             }
-            //Grow (the player cant be touching the ground or he will fall through it when he grows)
-            else if (!IsPlayerBig && !player.body.touching.down) {
-                IsPlayerBig = true;
-                player.setDisplaySize((originalPlayerWidth + 20), (originalPlayerHeight + 20));
+            else {
+                player.anims.play('attack1-right');
             }
         }
     }
